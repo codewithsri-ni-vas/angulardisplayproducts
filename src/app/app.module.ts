ade81bcf7fa5d08b6488e96b577336ccabfa42d8
sri-ni-vas <srinivas.ibo.58@gmail.com>
2022-01-30 12:53:56 -0800

    add product components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductdetailsComponent } from './components/productdetails/productdetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductdetailsComponent
   ],
   imports: [
     BrowserModule
